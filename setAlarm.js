4b167240a4c6dae9261c3ac113b230d8fc7536e2
ion-golovco <gaminglions2@gmail.com>
2019-01-09 16:39:53 +0200

    Add files via upload

---
+++ setAlarm.js
@@ -0,0 +1,9 @@
+function setAlarm(employed, vacation){
+    if(employed==vacation){
+        return false
+    }else if(vacation==true){
+        return false
+    }else{
+        return true
+    }
+}
