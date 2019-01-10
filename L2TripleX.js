4c830a2e440fbf160f454c66adad25d66c12743d
ion-golovco <gaminglions2@gmail.com>
2019-01-10 17:53:41 +0200

    Add files via upload

---
+++ L2TripleX.js
@@ -0,0 +1,16 @@
+function tripleX(str) {
+    if (str.match("x") == "x") {
+        for (let i = 0; i < str.length; i += 1) {
+            if (str[i] === "x") {
+                if (str[i] == str[i + 1] && str[i + 1] == str[i + 2]) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+    } else {
+        return false
+    }
+}
+console.log(tripleX("abraa"))
