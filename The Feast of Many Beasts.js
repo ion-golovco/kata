1543dee3e47e3c1b71a14bed617a19663692da35
ion-golovco <gaminglions2@gmail.com>
2019-01-13 00:09:23 +0200

    Add files via upload

---
+++ The Feast of Many Beasts.js
@@ -0,0 +1,10 @@
+function feast(beast, dish) {
+    return (beast[0]==dish[0]&&beast[beast.length-1]==dish[dish.length-1]?true : false)
+}
+function feast(beast, dish) {
+    if(beast[0]==dish[0]&&beast[beast.length-1]==dish[dish.length-1]){
+        return true
+    }else {
+        return false
+    }
+}
