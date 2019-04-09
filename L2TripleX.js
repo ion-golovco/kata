4f7b1e87ae78ddf0c01c3c838fd082342fc2204b
ion-golovco <gaminglions2@gmail.com>
2019-04-09 14:36:26 +0300

    Update L2TripleX.js

--- L2TripleX.js
+++ L2TripleX.js
@@ -2,7 +2,7 @@ function tripleX(str) {
     if (str.match("x") == "x") {
         for (let i = 0; i < str.length; i += 1) {
             if (str[i] === "x") {
-                if (str[i] == str[i + 1] && str[i + 1] == str[i + 2]) {
+                if (str[i] == str[i + 1] && str[i] == str[i + 2]) {
                     return true
                 } else {
                     return false
